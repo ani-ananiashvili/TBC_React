e86ad583f7185ee9fb6d8885a65cc6e6eb5c2abb
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-09-25 19:23:27 +0400

    clear the main component with its styles (for new assignment)

--- src/Main/MainContent.jsx
+++ src/Main/MainContent.jsx
@@ -1,84 +1,8 @@
 import React from "react";
-import "./MainContent.css"; 
+import "./MainContent.css";
 
 function MainContent() {
-  return (
-    <main className="main-content">
-      <section>
-        <h2>More of what you like</h2>
-        <div className="images-grid">
-          <div className="photo-container">
-            <img src="/public/music3.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music2.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music3.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music2.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music3.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <hr className="divider" />
-
-      <section>
-        <h2>Recently Played</h2>
-        <div className="images-grid">
-          <div className="photo-container">
-            <img src="/public/music1.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music2.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music1.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music2.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-          <div className="photo-container">
-            <img src="/public/music1.png" alt="image" />
-            <div className="play-icon">
-              <img src="/public/play.png" alt="play-icon" />
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default MainContent;
