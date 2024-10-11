8f391a05deb25a73e6753fa6bf59ff2d8773f02f
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-12 00:05:47 +0400

    remove vit icon and edit layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,22 @@
-import PropTypes from 'prop-types';
-import Head from 'next/head';
+import PropTypes from "prop-types";
+import Head from "next/head";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
-import '../global.css'
-
+import "../global.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <link rel="icon" type="icon" href="/public/logo-soundcloud.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>SoundCloud</title>
+        <link rel="icon" href="/logo-soundcloud.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body>
         <Header />
-        <div id="root">{children}</div>
+        <div className="main-content" id="root">
+          {children}
+        </div>
         <Footer />
       </body>
     </html>
