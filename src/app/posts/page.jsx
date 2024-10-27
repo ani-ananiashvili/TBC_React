f598d1e30a3b1c1d510886a6069cc44f883f8cd8
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-27 19:02:39 +0400

    refactor(posts): change search component

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,4 +1,4 @@
-import SearchBar from "../components/SearchBar/SearchBar";
+import PostFilter from "../components/PostSearch/PostSearch";
 import PostList from "../components/PostList/PostList";
 import "./index.css";
 
@@ -10,14 +10,25 @@ export default async function PostsPage({ searchParams }) {
     url = `https://dummyjson.com/posts/search?q=${searchTerm}`;
   }
 
-  const res = await fetch(url);
-  const data = await res.json();
-  const posts = data.posts || [];
+  console.log("Fetching URL:", url); 
+
+  let posts = [];
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const data = await res.json();
+    posts = data.posts || [];
+  } catch (error) {
+    console.error("Failed to fetch posts:", error);
+    return <p>Error loading posts... Please try again later...</p>; 
+  }
 
   return (
     <div className="posts-page">
       <h1 className="page-title">Blog Posts</h1>
-      <SearchBar />
+      <PostFilter />
       {posts.length > 0 ? (
         <PostList posts={posts} />
       ) : (
