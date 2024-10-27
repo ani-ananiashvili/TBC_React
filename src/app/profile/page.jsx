0333326a827fe531cbfc4dc8fbe68e2be35d51a6
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-27 22:01:52 +0400

    refactor: I change  profile page with user information

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,7 +5,7 @@ function Profile() {
     <div className="profile-container">
       <div className="profile-header">
         <img
-          src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/459305409_1059122095722747_3578119924291447655_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFFWxdhkorxXAGN3MLDqorTYsidthu6XXNiyJ22G7pdc3eZ5SWFWSiCTgbeBDJ0OpZsEj4MX5pYD5OdMdINejJ_&_nc_ohc=mrfbU3MXJzIQ7kNvgFYEQQ1&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AR3JpQKs5IedxGGkniDR8hv&oh=00_AYAJqwLMhXF178WakcKRDUI_tuygIzjXn_DDpeaAFIL3Ug&oe=67037A0A"
+          src="https://dummyjson.com/icon/emilys/128"
           alt="Profile"
           className="profile-photo"
         />
@@ -13,16 +13,20 @@ function Profile() {
       </div>
       <form className="profile-form">
         <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input className="profile-input" type="text" id="name" value="Ani" readOnly />
+          <label htmlFor="firstName">First Name:</label>
+          <input className="profile-input" type="text" id="firstName" value="Emily" readOnly />
         </div>
         <div className="form-group">
-          <label htmlFor="surname">Surname:</label>
-          <input className="profile-input" type="text" id="surname" value="Ananiashvili" readOnly />
+          <label htmlFor="lastName">Last Name:</label>
+          <input className="profile-input" type="text" id="lastName" value="Johnson" readOnly />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input className="profile-input" type="email" id="email" value="ananiashvili.ani@tbcacademy.edu.ge" readOnly />
+          <input className="profile-input" type="email" id="email" value="emily.johnson@x.dummyjson.com" readOnly />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender">Gender:</label>
+          <input className="profile-input" type="text" id="gender" value="Female" readOnly />
         </div>
       </form>
     </div>
