de54980e31f72c65d2dcceb03ddc4742379cf20d
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-28 23:21:10 +0400

    feat: add posts update function in postActions.js

--- src/app/components/functions/postActions.js
+++ src/app/components/functions/postActions.js
@@ -1,11 +1,19 @@
-export const addPostToLocalStorage = (newPost, posts) => {
-  const updatedPosts = [newPost, ...posts];
+export const addPostToLocalStorage = (newPost, existingPosts) => {
+  const updatedPosts = [newPost, ...existingPosts];
   localStorage.setItem("posts", JSON.stringify(updatedPosts));
   return updatedPosts;
 };
 
-export const deletePostFromLocalStorage = (postId, posts) => {
-  const updatedPosts = posts.filter((post) => post.id !== postId);
+export const deletePostFromLocalStorage = (postId, existingPosts) => {
+  const updatedPosts = existingPosts.filter((post) => post.id !== postId);
+  localStorage.setItem("posts", JSON.stringify(updatedPosts));
+  return updatedPosts;
+};
+
+export const updatePostInLocalStorage = (updatedPost, existingPosts) => {
+  const updatedPosts = existingPosts.map((post) =>
+    post.id === updatedPost.id ? updatedPost : post
+  );
   localStorage.setItem("posts", JSON.stringify(updatedPosts));
   return updatedPosts;
 };
