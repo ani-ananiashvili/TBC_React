3f079db97a314e73c73d8a3a9e8ae446eb44b5d7
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-31 19:55:18 +0400

    feat: update ProductPage to load from local storage

--- src/app/(dashboard)/products/page.jsx
+++ src/app/(dashboard)/products/page.jsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import AddProductForm from "../../components/forms/AddProductForm";
 import SearchBar from "../../components/Search/SearchBar";
 import SortComponent from "../../components/Sort/SortComponent";
-import "./products.css";
 import Link from "next/link";
+import "./products.css";
 
 async function fetchProducts(searchTerm = "", sortOption = "") {
   let url = "https://dummyjson.com/products";
@@ -20,13 +24,29 @@ async function fetchProducts(searchTerm = "", sortOption = "") {
   return data.products || [];
 }
 
-export default async function ProductPage({ searchParams }) {
+export default function ProductPage({ searchParams }) {
   const { q: searchTerm = "", sortBy: sortOption = "" } = searchParams;
-  const products = await fetchProducts(searchTerm, sortOption);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const apiProducts = await fetchProducts(searchTerm, sortOption);
+      const savedProducts =
+        JSON.parse(localStorage.getItem("customProducts")) || [];
+      setProducts([...savedProducts, ...apiProducts]);
+    }
+    loadProducts();
+  }, [searchTerm, sortOption]);
+
+  const handleAddProduct = (newProduct) => {
+    setProducts((prevProducts) => [newProduct, ...prevProducts]);
+  };
 
   return (
     <div className="product-page container">
       <h1>Our Products</h1>
+
+      <AddProductForm onAddProduct={handleAddProduct} />
 
       <div className="search-sort-container">
         <SearchBar searchTerm={searchTerm} />
