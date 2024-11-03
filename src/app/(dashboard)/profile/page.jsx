dffd19002a2227a92eab498f4b4b62ef631b7360
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-03 18:15:41 +0400

    refactor: update the Profile Page to display user data

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,36 +1,22 @@
-import "./Profile.css";
+"use client";
 
-function Profile() {
+import { useUser } from '@auth0/nextjs-auth0/client';
+import './Profile.css'; 
+
+export default function ProfileClient() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <div className="profile-container">
-      <div className="profile-header">
-        <img
-          src="https://dummyjson.com/icon/emilys/128"
-          alt="Profile"
-          className="profile-photo"
-        />
-        <h1>Profile</h1>
+    user && (
+      <div className="profile-container">
+        <h1>User Profile</h1>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
       </div>
-      <form className="profile-form">
-        <div className="form-group">
-          <label htmlFor="firstName">First Name:</label>
-          <input className="profile-input" type="text" id="firstName" value="Emily" readOnly />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName">Last Name:</label>
-          <input className="profile-input" type="text" id="lastName" value="Johnson" readOnly />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input className="profile-input" type="email" id="email" value="emily.johnson@x.dummyjson.com" readOnly />
-        </div>
-        <div className="form-group">
-          <label htmlFor="gender">Gender:</label>
-          <input className="profile-input" type="text" id="gender" value="Female" readOnly />
-        </div>
-      </form>
-    </div>
+    )
   );
 }
-
-export default Profile;
