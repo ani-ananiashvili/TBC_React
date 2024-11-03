84a0c84014f6653abf57db74fef6470ac3f4eed6
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-03 17:46:56 +0400

    refactor: remove the custom login logic from the login page, replacing it with a call to the new login function from useAuth

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,43 +1,20 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import useAuth from "../../hooks/useAuth";
-import './Login.css'
+import './Login.css';
 
 export default function LoginPage() {
   const router = useRouter();
-  const { login } = useAuth();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { login, isAuthenticated } = useAuth();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const success = await login(username, password);
-    if (success) {
-      router.push("/home"); 
-    }
-  };
+  if (isAuthenticated) {
+    router.push("/home"); 
+  }
 
   return (
     <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+      <h1 className="welcome-message">WELCOME TO MY PROJECT!</h1>
+      <button className="login-button" onClick={login}>Login</button>
     </div>
   );
 }
