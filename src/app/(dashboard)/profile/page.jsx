4a39118654b72c9de9293604b4f0228e9a7072f7
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-05 20:00:13 +0400

    feat: update Profile page with Tailwind CSS

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import './Profile.css'; 
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function ProfileClient() {
   const { user, error, isLoading } = useUser();
@@ -11,11 +10,17 @@ export default function ProfileClient() {
 
   return (
     user && (
-      <div className="profile-container">
-        <h1>User Profile</h1>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div className="flex flex-col items-center justify-center min-h-[70vh] p-5 text-center">
+        <h1 className="text-2xl md:text-3xl font-semibold underline mb-5">
+          User Profile
+        </h1>
+        <img
+          src={user.picture}
+          alt={user.name}
+          className="w-24 h-24 md:w-32 md:h-32 rounded-full object-cover shadow-md mb-5"
+        />
+        <h2 className="text-xl md:text-2xl font-medium mb-2">{user.name}</h2>
+        <p className="text-sm">{user.email}</p>
       </div>
     )
   );
