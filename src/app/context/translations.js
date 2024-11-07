7cf6fa3de8036559cddb4283c844b812db3b48ac
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-07 23:51:04 +0400

    feat: create a translations.js file to store the text for English and Georgian (ka)

---
+++ src/app/context/translations.js
@@ -0,0 +1,23 @@
+export const translations = {
+    en: {
+      home: "Home",
+      profile: "Profile",
+      contact: "Contact",
+      about: "About",
+      login: "Login",
+      logout: "Logout",
+      signIn: "Sign In",
+      signUp: "Sign Up",
+    },
+    ka: {
+      home: "მთავარი",
+      profile: "პროფილი",
+      contact: "კონტაქტი",
+      about: "ჩვენ შესახებ",
+      login: "შესვლა",
+      logout: "გასვლა",
+      signIn: "შესვლა",
+      signUp: "რეგისტრაცია",
+    },
+  };
+  
