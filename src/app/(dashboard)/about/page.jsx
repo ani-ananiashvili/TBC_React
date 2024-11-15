9662d7f3c3175c68205c4eee00b8d5b9b062f863
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-15 18:57:30 +0400

    chore: delete unnecessary files

--- src/app/(dashboard)/about/page.jsx
+++ src/app/(dashboard)/about/page.jsx
@@ -1,32 +1,7 @@
 import "./about.css";
 
 function About() {
-  return (
-    <div className="about-container">
-      <h1>What is SoundCloud?</h1>
-      <p>
-        Founded in 2007, SoundCloud is an artist-first platform powered by a
-        global community of artists and listeners on the pulse of what’s new,
-        now and next in music culture. We empower independent artists with the
-        tools, services and resources they need to help them build and grow
-        their careers. With 400+ million tracks from 40+ million artists from
-        193 countries on SoundCloud, we are the largest audio discovery platform
-        in the world.
-      </p>
-      <h2>How can you use SoundCloud?</h2>
-      <p>
-        Be an artist. Upload tracks and share them with your social networks.
-        Promote your releases, engage with fans and earn money when you become
-        eligible to monetize. Create an account for free or upgrade to Next Pro
-        to access our best-in-class tools. Check out our artist resource center
-        here. Be a listener. Find new music and share favorite discoveries with
-        your social networks. Support the artists you love by commenting,
-        reposting and liking their tracks. You can even save unlimited tracks
-        and playlists for ad-free, offline listening with a Go+ subscription.
-        Learn how to optimize your listening experience here.
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default About;
--- src/app/(dashboard)/blog/page.jsx
+++ src/app/(dashboard)/blog/page.jsx
@@ -1,43 +1,7 @@
 import "./Blog.css";
 
 function Blog() {
-  const posts = [
-    {
-      title: "WHAT'S NEW WITH SOUNDCLOUD - September 2024",
-      content:
-        "It's September and as we embrace the change of seasons and the return to busy schedules, we're excited to announce new features that keep you connected to your favorite artists and tracks. Whether you’re settling into a new routine, discovering fresh sounds, or just returning to normal, read on to discover what’s new this month.",
-      imageURL:
-        "https://cdn.prod.website-files.com/62a0a0168756b7e12bbc65d3/66fb374fe50927d0676a8d38_PMM_ProductUpdate_Sep_Blog_Editorialthumbnail-wLogo.jpg",
-    },
-    {
-      title: "WHAT'S NEW WITH SOUNDCLOUD - August 2024",
-      content:
-        "This August, we launched a range of new features on SoundCloud. Whether you're squeezing in a last-minute vacation, gearing up for the new school year, or just keeping to your routine, here are some product highlights from this month to help you stay connected to the music and artists you love.",
-      imageURL:
-        "https://cdn.prod.website-files.com/62a0a0168756b7e12bbc65d3/66d73cd18e43dd2d599398a9_PMM_ProductUpdate_Aug_Blog_Editorialthumbnail-wLogo-p-500.jpg",
-    },
-    {
-      title: "WHAT'S NEW WITH SOUNDCLOUD - July 2024",
-      content:
-        "This July, we’re bringing the sunshine to your SoundCloud experience. Read on to explore new features that are sure to light up your summer sound.",
-      imageURL:
-        "https://cdn.prod.website-files.com/62a0a0168756b7e12bbc65d3/66aacf07ecd2dd631f8b965b_PMM_ProductUpdate_July_Blog_Editorialthumbnail-wLogo%20(1)-p-500.jpg",
-    },
-  ];
-
-  return (
-    <div className="blog-container">
-      {posts.map((post, index) => (
-        <div key={index} className="blog-post">
-          <img src={post.imageURL} alt={post.title} className="post-image" />
-          <div className="post-content">
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Blog;
