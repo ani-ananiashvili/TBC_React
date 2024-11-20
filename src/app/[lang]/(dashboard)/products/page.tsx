01321d9e8c1d58e9a5d120f56ec3102b821624d0
nikakaladze <nikakaladze89@gmail.com>
2024-11-21 01:46:38 +0400

    add map function to product

--- src/app/[lang]/(dashboard)/products/page.tsx
+++ src/app/[lang]/(dashboard)/products/page.tsx
@@ -1,14 +1,29 @@
 import Link from "next/link";
-interface Products{
-  id:number,
-  title:string,
-  description:string,
-
+interface Products {
+  id: number;
+  Title: string;
+  Description: string;
+  Price: number;
 };
 
 
 
 export default async function ProductsPage() {
   
+const response = await fetch("http://localhost:3000/api/products");
+const products:Products[] = await response.json();
+
+return (
+  <div>
+    <h1>Products</h1>
+    {products.map((product) => (
+      <div key={product.id} className="">
+        <h2>{product.Title}</h2>
+        <p>{product.Description}</p>
+        <p>Price: {product.Price}</p>
+      </div>
+    ))}
+  </div>
+);
 
 }
