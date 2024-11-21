b90576ca70ca8e4a8aa495d22d6c04fc823f9440
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-11-21 19:41:36 +0400

    refactor(login): update the design

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -1,20 +1,34 @@
 "use client";
 import { useRouter } from "next/navigation";
 import useAuth from "../../../hooks/useAuth";
-import './Login.css';
+import "./login.css";
 
 export default function LoginPage() {
   const router = useRouter();
   const { login, isAuthenticated } = useAuth();
 
   if (isAuthenticated) {
-    router.push("/home"); 
+    router.push("/home");
   }
 
   return (
     <div className="login-container">
-      <h1 className="welcome-message">WELCOME TO MY PROJECT!</h1>
-      <button className="login-button" onClick={login}>Login</button>
+      <div className="background-image"></div>
+      <div className="login-content">
+        <h1 className="welcome-message">Welcome to the Furniture Store!</h1>
+        <p className="store-description">
+          Discover stylish and comfortable furniture for every room in your
+          home. From modern to classic, we have something for every taste.
+        </p>
+        <p className="store-benefits">
+          Enjoy exclusive deals, free delivery, and quality craftsmanship that
+          lasts.
+        </p>
+        <button className="login-button" onClick={login}>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
+
