1407b88eafb5fd4c563451c4cf0a01c17c998423
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-21 21:47:05 +0400

    fixed: products
    1. Updated api/productsand posts/products to dynamically update/fetch language specific data;
    2. Added Image fetching.

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,20 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import supabase from "../../components/utils/supabase";
 
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-}
-
 export async function GET(req: NextRequest): Promise<NextResponse> {
   const language = req.cookies.get("language")?.value || "en";
 
   const columns =
     language === "ka"
-      ? "id, Title_Ka, Description_Ka, Price"
-      : "id, Title, Description, Price";
+      ? "id, Title_Ka, Description_Ka, Price, Image"
+      : "id, Title, Description, Price, Image";
 
   const { data: products, error } = await supabase.from("Products").select(columns);
 
