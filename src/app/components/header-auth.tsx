0e7b585b111c560db5aa8f14a242450920e651bd
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-04 01:25:54 +0400

    feat: add UI components for form messages, warnings, and authentication buttons

---
+++ src/app/components/header-auth.tsx
@@ -0,0 +1,69 @@
+import { signOutAction } from "../actions";
+import { Client } from "../../../utils/supabase/client";
+import Link from "next/link";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+import { createClient } from "../../../utils/supabase/server";
+
+export default async function AuthButton() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!Client) {
+    return (
+      <>
+        <div className="flex gap-4 items-center">
+          <div>
+            <Badge
+              variant={"default"}
+              className="font-normal pointer-events-none"
+            >
+            </Badge>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              asChild
+              size="sm"
+              variant={"outline"}
+              disabled
+              className="opacity-75 cursor-none pointer-events-none"
+            >
+              <Link href="/sign-in">Sign in</Link>
+            </Button>
+            <Button
+              asChild
+              size="sm"
+              variant={"default"}
+              disabled
+              className="opacity-75 cursor-none pointer-events-none"
+            >
+              <Link href="/sign-up">Sign up</Link>
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hey, {user.email}!
+      <form action={signOutAction}>
+        <Button type="submit" variant={"outline"}>
+          Sign out
+        </Button>
+      </form>
+    </div>
+  ) : (
+    <div className="flex gap-2">
+      <Button asChild size="sm" variant={"outline"}>
+        <Link href="/sign-in">Sign in</Link>
+      </Button>
+      <Button asChild size="sm" variant={"default"}>
+        <Link href="/sign-up">Sign up</Link>
+      </Button>
+    </div>
+  );
+}
