a4cc77866ac2df0f773fe4c042f318182f4317cf
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-04 01:40:45 +0400

    refactor: improve layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,43 @@
-"use client";
-
-import PropTypes from "prop-types";
-import "/global.css";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { ThemeProvider } from "./context/ThemeContext";
 import { LanguageProvider } from "./context/LanguageContext";
+import Footer from "./components/Footer/Footer";
+import { EnvVarWarning } from "./components/env-var-warning";
+import HeaderAuth from "./components/header-auth";
+import { Client } from "../../utils/supabase/client";
+import "/global.css";
 
-interface RootLayoutProps {
-  children?: React.ReactNode;
-}
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
-      <UserProvider>
-        <ThemeProvider>
-          <LanguageProvider>
-            <body>{children}</body>
-          </LanguageProvider>
-        </ThemeProvider>
-      </UserProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-background text-foreground">
+        <LanguageProvider>
+          <main className="min-h-screen flex flex-col items-center">
+            <div className="w-full">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                  <div className="flex gap-5 items-center font-semibold">
+                    <div className="flex items-center gap-2"></div>
+                  </div>
+                  {!Client ? <EnvVarWarning /> : <HeaderAuth />}
+                </div>
+              </nav>
+            </div>
+
+            <div className="flex flex-col gap-20 max-w-5xl p-5 w-full">
+              {children}
+            </div>
+
+            <Footer />
+          </main>
+        </LanguageProvider>
+      </body>
     </html>
   );
 }
-
-RootLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
