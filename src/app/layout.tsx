a14bf6a4832843a1da52d2e42b42c489745dcb49
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-04 22:56:41 +0400

    refactor: modify layout to use Header and ThemeContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import { LanguageProvider } from "./context/LanguageContext";
 import Footer from "./components/Footer/Footer";
 import { EnvVarWarning } from "./components/env-var-warning";
-import HeaderAuth from "./components/header-auth";
+import Header from "./components/Header/Header";
 import { Client } from "../../utils/supabase/client";
+import { ThemeProvider } from "./context/ThemeContext";
 import "/global.css";
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
 
 export default function RootLayout({
   children,
@@ -18,24 +15,24 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <LanguageProvider>
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="w-full">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <div className="flex items-center gap-2"></div>
+          <ThemeProvider>
+            <main className="min-h-screen flex flex-col items-center">
+              <div className="w-full">
+                <nav>
+                  <div>
+                    <Header />
+                    {!Client ? <EnvVarWarning /> : null}
                   </div>
-                  {!Client ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-            </div>
+                </nav>
+              </div>
 
-            <div className="flex flex-col gap-20 max-w-5xl p-5 w-full">
-              {children}
-            </div>
+              <div className="flex flex-col gap-20 max-w-5xl p-5 w-full">
+                {children}
+              </div>
 
-            <Footer />
-          </main>
+              <Footer />
+            </main>
+          </ThemeProvider>
         </LanguageProvider>
       </body>
     </html>
