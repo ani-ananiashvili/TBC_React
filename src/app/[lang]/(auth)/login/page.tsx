99799678e95ed1001d1e1c4331e415ecc73e01d5
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-05 23:07:31 +0400

    style(Login): update auth styles

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 export default async function Login({ searchParams }: Props) {
   return (
-    <div className="flex gap-8 justify-center mt-16 flex-col md:flex-row">
-      <div className="flex-1">
+    <div className="flex gap-4 justify-center mt-16 mb-16 flex-col md:flex-row">
+      <div className="flex-1 max-w-sm">
         <SignInForm searchParams={searchParams} />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 max-w-sm">
         <SignUpForm searchParams={searchParams} />
       </div>
     </div>
--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -9,7 +9,7 @@ interface SignUpFormProps {
 
 export default function SignUpForm({ searchParams }: SignUpFormProps) {
   return (
-    <form className="flex flex-col min-w-64 max-w-64 mx-auto">
+    <form className="flex flex-col min-w-64 max-w-xs mx-auto border border-[#4a628a] rounded-lg p-6 h-full">
       <h1 className="text-2xl font-medium">Sign up</h1>
       <p className="text-sm text-foreground">
         Already have an account?{" "}
@@ -19,16 +19,26 @@ export default function SignUpForm({ searchParams }: SignUpFormProps) {
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <label htmlFor="email">Email</label>
-        <input name="email" placeholder="you@example.com" required />
+        <input
+          className="border border-gray-300 rounded p-2 focus:outline-none focus:ring-2 focus:border-blue-500"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
         <label htmlFor="password">Password</label>
         <input
+          className="border border-gray-300 rounded p-2 focus:outline-none focus:ring-2 focus:border-blue-500"
           type="password"
           name="password"
           placeholder="Your password"
           minLength={6}
           required
         />
-        <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+        <SubmitButton
+          className="bg-[#4a628a] hover:bg-[#3e4f6a] text-white rounded p-2 mt-4"
+          formAction={signUpAction}
+          pendingText="Signing up..."
+        >
           Sign up
         </SubmitButton>
         <FormMessage message={searchParams} />
