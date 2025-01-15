75ffdb78a90bb6ade3ae987c1ef881b91dacf707
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-15 17:25:27 +0400

    fix: update Login component to pass correctly typed searchParams to SignInForm

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -2,11 +2,10 @@ import SignInForm from "../../../components/SignInForm";
 import SignUpForm from "../../../components/SignUpForm";
 
 interface Props {
-  searchParams: Promise<Record<string, string | undefined>>; 
+  searchParams: Record<string, string | undefined>;
 }
 
-export default async function Login(props: Props) {
-  const searchParams = await props.searchParams;
+export default function Login({ searchParams }: Props) {
   return (
     <div className="flex gap-4 justify-center mt-16 mb-16 flex-col md:flex-row">
       <div className="flex-1 max-w-sm">
