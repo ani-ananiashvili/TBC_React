cf7f18e972867c0a4875a194cf444d438e7c085a
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-29 00:17:28 +0400

    feat: create CartContext for managing cart state
    - Add CartContext.tsx to manage cart state.
    - Implement logic to add and remove products from the cart.
    - Store cart data in localStorage.

---
+++ src/app/context/CartContext.tsx
@@ -0,0 +1,74 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  photo: string;
+  quantity: number;
+  description: string;
+}
+
+interface CartContextProps {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (id: string) => void;
+  cartCount: number;
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product: Product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((p) => p.id === product.id);
+      if (existingProduct) {
+        return prevCart.map((p) =>
+          p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (id: string) => {
+    setCart((prevCart) => prevCart.filter((product) => product.id !== id));
+  };
+
+  const cartCount = cart.reduce((acc, product) => acc + product.quantity, 0);
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, cartCount }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCartContext must be used within a CartProvider");
+  }
+  return context;
+};
