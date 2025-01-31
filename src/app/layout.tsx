4e872a4cffc96e20f6623e4bbb75e33efc7ca200
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-01-31 23:58:04 +0400

    feat: add Stripe purchase functionality to product

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,30 +8,19 @@ import { ThemeProvider } from "./context/ThemeContext";
 import "/global.css";
 import { usePathname } from "next/navigation";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  const isAuthRoute =
-    pathname.includes("/sign-in") || pathname.includes("/sign-up");
+  const isAuthRoute = pathname.includes("/sign-in") || pathname.includes("/sign-up");
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        style={{
-          background: "linear-gradient(145deg, #f5f7fa, #e4ebf5)",
-        }}
-      >
+      <body style={{ background: "linear-gradient(145deg, #f5f7fa, #e4ebf5)" }}>
         <LanguageProvider>
           <ThemeProvider>
             <CartProvider>
               <nav>{!isAuthRoute && <Header />}</nav>
-              <main>
-                <div>{children}</div>
-              </main>
+              <main>{children}</main>
               <Footer />
             </CartProvider>
           </ThemeProvider>
