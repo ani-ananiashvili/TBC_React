d4574a6f11ad290d8f42e015d8716931a904bf90
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-02-01 20:03:51 +0400

    feat: add order details functionality for product purchase

--- src/app/api/buy-furniture-product/route.ts
+++ src/app/api/buy-furniture-product/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: Request) {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success-checkout`,
+      success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success-checkout?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/cancel`,
     });
 
