c3a812a863be21b3c60b9c69264bdf688587d449
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-02-05 01:03:09 +0400

    feat(product-form): add validation and language support

--- src/app/[lang]/(dashboard)/create-furniture-product/page.tsx
+++ src/app/[lang]/(dashboard)/create-furniture-product/page.tsx
@@ -1,15 +1,76 @@
 "use client";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useLanguageContext } from "../../../context/LanguageContext";
 
 export default function CreateProductForm() {
   const [message, setMessage] = useState<string | null>(null);
   const [isError, setIsError] = useState<boolean>(false);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const router = useRouter();
+  const { language } = useLanguageContext();
 
-  const categories = ["Knoll", "Vitra", "Cassina"];
+  const categories = [
+    "Herman Miller",
+    "Ashley",
+    "Knoll",
+    "Vitra",
+    "BoConcept",
+    "Cassina",
+    "Natuzzi",
+    "Restoration Hardware",
+    "West Elm",
+    "Roche Bobois",
+
+    "IKEA",
+    "Flexform",
+  ];
+
+  // validation
+
+  const validateForm = (
+    name: string,
+    price: number,
+    photo: string,
+    description: string
+  ) => {
+    const nameRegex = /^[a-zA-Z0-9 ]{3,50}$/;
+    const priceRegex = /^[0-9]+(\.[0-9]{1,2})?$/;
+    const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/;
+    const descriptionRegex = /^.{10,500}$/;
+
+    if (!name || !nameRegex.test(name)) {
+      return language === "ka"
+        ? "უსაფრთხო პროდუქტის დასახელება, შეიყვანეთ სათაური 3-დან 50 სიმბოლომდე."
+        : "Invalid product title. Please enter a title between 3 and 50 characters.";
+    }
+
+    if (!price || !priceRegex.test(price.toString())) {
+      return language === "ka"
+        ? "შეიყვანეთ სწორი ფასი."
+        : "Please enter a valid price.";
+    }
+
+    if (photo && !urlRegex.test(photo)) {
+      return language === "ka"
+        ? "მიუღებელი ფოტო URL."
+        : "Invalid photo URL format.";
+    }
+
+    if (!description || !descriptionRegex.test(description)) {
+      return language === "ka"
+        ? "აღწერა უნდა იყოს 10-დან 500 სიმბოლომდე."
+        : "Description should be between 10 and 500 characters.";
+    }
+
+    if (!selectedCategory) {
+      return language === "ka"
+        ? "გთხოვთ, შეარჩიეთ ბრენდი."
+        : "Please select a brand.";
+    }
+
+    return null;
+  };
 
   async function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
@@ -19,6 +80,14 @@ export default function CreateProductForm() {
     const photo = formData.get("photo") as string;
     const brand = selectedCategory;
     const description = formData.get("description") as string;
+
+    const validationMessage = validateForm(name, price, photo, description);
+
+    if (validationMessage) {
+      setMessage(validationMessage);
+      setIsError(true);
+      return;
+    }
 
     const productData = { name, price, photo, brand, description };
 
@@ -48,27 +117,27 @@ export default function CreateProductForm() {
   }
 
   return (
-    <div className="m-24 max-w-2xl mx-auto space-y-4">
+    <div className="p-24 pb-16 min-h-screen bg-light-gradient dark:bg-dark-gradient">
       <div className="text-center">
         <img
           src="/assets/logo_aveji.png"
           alt="logo"
-          className="mx-auto mb-4 w-28 bg-gray-600"
+          className="mx-auto mb-4 w-20 bg-transparent bg-sky-600"
         />
-        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-          Add New Product
+        <h2 className="text-2xl font-semibold text-[#4a628a] mb-6">
+          {language === "ka" ? "დაამატეთ ახალი პროდუქტი" : "Add New Product"}
         </h2>
       </div>
 
       <form
         onSubmit={handleSubmit}
-        className="space-y-4 p-6 bg-gray-600 shadow-md rounded-lg"
+        className="space-y-4 p-6 max-w-2xl mx-auto bg-white shadow-md rounded-lg border border-[#4a628a] dark:bg-dark-gradient dark:border-gray-600"
       >
         {message && (
           <div
-            className={`p-4 mb-4 text-white ${
-              isError ? "bg-red-500" : "bg-green-500"
-            }`}
+            className={`p-4 mb-4 ${
+              isError ? "text-red-600" : "bg-green-600"
+            } rounded-md`}
           >
             {message}
           </div>
@@ -77,56 +146,64 @@ export default function CreateProductForm() {
         <div>
           <label
             htmlFor="name"
-            className="block text-lg font-medium text-gray-700"
+            className="block text-lg font-medium text-[#4a628a]"
           >
-            Product Title:
+            {language === "ka" ? "პროდუქტის დასახელება" : "Product Title"}:
           </label>
           <input
             type="text"
             id="name"
             name="name"
-            placeholder="Enter product title"
-            className="w-full p-2 mt-1 border border-gray-300 outline-none"
+            placeholder={
+              language === "ka"
+                ? "შეიყვანეთ პროდუქტის დასახელება"
+                : "Enter product title"
+            }
+            className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#4a628a] outline-none dark:bg-dark-gradient dark:text-white"
           />
         </div>
 
         <div>
           <label
             htmlFor="description"
-            className="block text-lg font-medium text-gray-700"
+            className="block text-lg font-medium text-[#4a628a]"
           >
-            Product Description:
+            {language === "ka" ? "პროდუქტის აღწერა" : "Product Description"}:
           </label>
           <textarea
             id="description"
             name="description"
-            placeholder="Enter product description"
-            className="w-full p-2 mt-1 border border-gray-300 outline-none"
+            placeholder={
+              language === "ka"
+                ? "შეიყვანეთ პროდუქტის აღწერა"
+                : "Enter product description"
+            }
+            className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#4a628a] outline-none dark:bg-dark-gradient dark:text-white"
           />
         </div>
 
         <div>
           <label
             htmlFor="price"
-            className="block text-lg font-medium text-gray-700"
+            className="block text-lg font-medium text-[#4a628a]"
           >
-            Price:
+            {language === "ka" ? "ფასი" : "Price"}:
           </label>
           <input
             type="number"
             id="price"
             name="price"
-            placeholder="Enter price"
-            className="w-full p-2 mt-1 border border-gray-300 outline-none"
+            placeholder={language === "ka" ? "შეიყვანეთ ფასი" : "Enter price"}
+            className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#4a628a] outline-none dark:bg-dark-gradient dark:text-white"
           />
         </div>
 
         <div className="space-y-2">
           <label
             htmlFor="category"
-            className="block text-lg font-medium text-gray-700"
+            className="block text-lg font-medium text-[#4a628a]"
           >
-            Product Brand:
+            {language === "ka" ? "პროდუქტის ბრენდი" : "Product Brand"}:
           </label>
           <div className="flex flex-wrap gap-4">
             {categories.map((category) => (
@@ -134,10 +211,10 @@ export default function CreateProductForm() {
                 key={category}
                 type="button"
                 onClick={() => setSelectedCategory(category)}
-                className={`px-4 py-2 rounded-md border ${
+                className={`px-4 py-2 rounded-md border text-lg font-medium ${
                   selectedCategory === category
-                    ? "bg-sky-700 text-white"
-                    : "bg-gray-300 text-gray-700"
+                    ? "bg-[#4a628a] text-white"
+                    : "bg-gray-200 text-[#4a628a] hover:bg-[#e4ebf5]"
                 }`}
               >
                 {category}
@@ -145,8 +222,9 @@ export default function CreateProductForm() {
             ))}
           </div>
           {selectedCategory && (
-            <p className="mt-2 text-sm text-gray-600">
-              Selected Brand: {selectedCategory}
+            <p className="mt-2 text-sm text-[#4a628a]">
+              {language === "ka" ? "არჩეული ბრენდი: " : "Selected Brand: "}{" "}
+              {selectedCategory}
             </p>
           )}
         </div>
@@ -154,34 +232,36 @@ export default function CreateProductForm() {
         <div>
           <label
             htmlFor="photo"
-            className="block text-lg font-medium text-gray-700"
+            className="block text-lg font-medium text-[#4a628a]"
           >
-            Photo Upload:
+            {language === "ka" ? "ფოტოს ატვირთვა" : "Photo Upload"}:
           </label>
           <div className="flex items-center space-x-2">
             <input
               type="file"
               id="photo"
               name="photo"
-              className="border border-gray-300 p-2"
+              className="border border-gray-300 p-3 rounded-lg"
               disabled
             />
-            <span className="text-gray-700">or</span>
+            <span className="text-[#4a628a]">or</span>
             <input
               type="url"
               id="photo"
               name="photo"
-              placeholder="Enter photo URL"
-              className="w-full p-2 mt-1 border border-gray-300 outline-none"
+              placeholder={
+                language === "ka" ? "შეიყვანეთ ფოტო URL" : "Enter photo URL"
+              }
+              className="w-full p-3 mt-1 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#4a628a] outline-none dark:bg-dark-gradient dark:text-white"
             />
           </div>
         </div>
 
         <button
           type="submit"
-          className="w-full py-2 px-4 bg-white text-black font-bold rounded-md"
+          className="w-full py-3 px-4 bg-[#4a628a] text-white font-bold rounded-md hover:bg-[#365f80] dark:bg-[#365f80] dark:hover:bg-[#4a628a]"
         >
-          Create Product
+          {language === "ka" ? "პროდუქტის დამატება" : "Add Product"}
         </button>
       </form>
     </div>
