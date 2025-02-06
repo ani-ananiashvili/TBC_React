a7e17538e69892777f4f75c2bb0f22a98e8e70ac
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-02-06 22:49:17 +0400

    fix: dynamic route handler to correctly access params in API route

--- src/app/api/get-products/[id]/route.ts
+++ src/app/api/get-products/[id]/route.ts
@@ -1,23 +1,31 @@
 import { NextResponse } from "next/server";
 import { createClient } from "../../../utils/supabase/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: Request, context: { params: { id: string } }) {
   try {
+    const { id } = context.params;
+
     const supabase = await createClient();
     const { data, error } = await supabase
       .from("Furniture_Products")
       .select("*")
-      .eq("id", params.id)
+      .eq("id", id)
       .single();
 
     if (error) {
       console.error("Error fetching product:", error.message);
-      return NextResponse.json({ message: "Error fetching product" }, { status: 400 });
+      return NextResponse.json(
+        { message: "Error fetching product" },
+        { status: 400 }
+      );
     }
 
     return NextResponse.json(data);
   } catch (error) {
     console.error("Failed to fetch product:", error);
-    return NextResponse.json({ message: "Failed to fetch product" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Failed to fetch product" },
+      { status: 500 }
+    );
   }
 }
