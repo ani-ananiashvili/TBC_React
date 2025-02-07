8cd2ac5f56a695173c19b32ac4de9a17c5a6a1cc
Ani Ananiashvili <ananiashvili10@gmail.com>
2025-02-07 17:37:20 +0400

    feat: make header and footer responsive for small screens

--- src/app/components/Header/AuthHeader.tsx
+++ src/app/components/Header/AuthHeader.tsx
@@ -3,6 +3,7 @@
 import { useThemeContext } from "../../context/ThemeContext";
 import { useLanguageContext } from "../../context/LanguageContext";
 import "./AuthHeader.css";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const AuthHeader = (): JSX.Element => {
   const { theme, changeTheme } = useThemeContext();
@@ -11,7 +12,7 @@ const AuthHeader = (): JSX.Element => {
   return (
     <header className="header auth-header">
       <div className="logo">
-        <img src="/assets/logo_aveji.png" alt="logo" />
+        <img src="/assets/logo_furniture.png" alt="logo" />
       </div>
       <div className="controls">
         <div className="theme-toggle">
@@ -19,7 +20,11 @@ const AuthHeader = (): JSX.Element => {
             className="toggle-track"
             onClick={() => changeTheme(theme === "light" ? "dark" : "light")}
           >
-            {theme === "light" ? "⛅" : "🌙"}
+            {theme === "light" ? (
+              <FiSun title="Light Mode" />
+            ) : (
+              <FiMoon title="Dark Mode" />
+            )}
           </div>
         </div>
         <div className="language-toggle">
